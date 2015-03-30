d9d4cdc12074c4a56a0a6aee1505d1dfd7839d4a
eloises05 <s1200286@connect.glos.ac.uk>
2015-03-30 19:50:37 +0100

    insert statemens pbis 2-5

---
+++ SQL Scripts/Inserts/insert statements pbis 2-5.sql
@@ -0,0 +1,18 @@
+INSERT INTO Backlog_Items
+VALUES
+
+(NULL, 'Create PBI Form', 'Pay attention to date formats during creation', NULL, '3', '1', NULL, '1'),
+(NULL, 'Create Task Form', 'Project team enter task details here', NULL, '3', '1', NULL, '1'),
+(NULL, 'Create Form Validation', 'Enable simple inputs with reduced errors', NULL, '3', '1', NULL, '1'),
+(NULL, 'Design The Forms', 'Decide on standard format and design of data entry', NULL, '3', '1', NULL, '1'),
+(NULL, 'Plan Nullable Fields', NULL', 'NULL', '2', '1', '1', '1'),
+(NULL, 'Thoroughly go through validation using Test Script', 'Confirm error reduction', 'NULL', '2', '1', NULL, '1'),
+(NULL, 'Trigger for audit table', 'Create automated process', NULL, '2', '1', NULL, '1'),
+(NULL, 'Create audit table for changes to PBI's and Tasks', 'Table to show changes made by team', NULL, '2', '1', NULL, '1'),
+(NULL, 'Create comments table for PBI', 'enable team to explain progress/ what has been done', '1', '1', '1', '1', '1'),
+(NULL, 'Create comments table for tasks', 'enable team to explain progress', '1', '1', '1', '1', '1'),
+(NULL, 'Design UI for assigning Effort and Time', 'Decide on drop down menu/sliders', NULL, '3', '1', NULL, '1'),
+(NULL, 'Integrate value into database', NULL, NULL, '3', '1', NULL, '1'),
+(NULL, 'Design UI for choosing status', NULL, NULL, '3', '1', NULL, '1'),
+(NULL, 'Choose status', 'decide standard terms', '1', '2', '1', NULL '1'),
+(NULL, 'Integrate with database' 'show changes/choices in database', NULL, '1', '1', NULL, '1')
