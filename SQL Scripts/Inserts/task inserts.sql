c9bea6d9ed32f392f913f610c1dd6dceca124974
eloises05 <s1200286@connect.glos.ac.uk>
2015-04-28 14:46:00 +0100

    task inserts

---
+++ SQL Scripts/Inserts/task inserts.sql
@@ -0,0 +1,53 @@
+/* Task Table Inserts*/
+/* Task ID, Task Title, Task Description, Task Estimated Duration, Task hours done, Assignee, State_ID, Iteration_ID, PBI ID */
+INSERT INTO Task
+VALUES
+(NULL, 'Create Content Map', 'Decide where content will go in terms of website layout', '2.00', NULL, 'KM', '7', '2', '1'),
+(NULL, 'Create Sitemap', 'Decide on website layout', '2.00', NULL, 'KM', '7', '2', '1'),
+(NULL, 'Identify number of static templates', NULL, NULL, NULL, '7', NULL, NULL, '2'),
+(NULL, 'Create wireframe for template 1', NULL, NULL, NULL, '7', NULL, NULL, '2'),
+(NULL, 'Create wireframe for template 2', NULL, NULL, NULL, '7', NULL, NULL, '2
+(NULL, 'Create wireframe for template 3', NULL, NULL, NULL, '7', NULL, NULL, '2'),
+(NULL, 'Create wireframe for template 4', NULL, NULL, NULL, '7', NULL, NULL, '2'),
+(NULL, 'Choose branding colours', 'Discuss and decide on set colour template for Zeus brand', '2.00', '3.00', 'KM', '10', '1', '3'),
+(NULL, 'Design logo', 'Create Zeus brand logo', '5.00', '4.00', 'KM', '10', '1', '3'),
+(NULL, 'Choose branding fonts', 'Decide on font to be consistently used in Zeus brand', '2.00', '2.00', 'KM', '10', '1', '3'),
+(NULL, 'Write document', 'Complete Zeus brand document', '13.50', '16.00', 'KM', '8', '3', '3'),
+(NULL, 'Company mission statement', 'Decide on Zeus mission statement', '1.00', NULL, 'DN', '7', '1', '3'),
+(NULL, 'Company etiquette', NULL, '2.00', NULL, 'DN', '7', '1', '3'),
+(NULL, 'Provide resized logo images for social media', 'Resize logo for cover images and display pictures', '1.00', '0.50', 'KM', '10', '1', '3'),
+(NULL, 'Edit Brand ID document text', 'Check brand document for correct grammar and spelling', '2.00', NULL, 'ES', '7', '3', '3'),
+(NULL, 'Create visual template for wireframe template 1', NULL, NULL, NULL, NULL, '7', NULL, '4'),
+(NULL, 'Create visual template for wireframe template 2', NULL, NULL, NULL, NULL, '7', NULL, '4'),
+(NULL, 'Critique Competitor's websites', 'Find and discuss good and bad aspects of similar company websites', NULL, NULL, NULL, '7', NULL, '5'),
+(NULL, 'Decide on Bootstrap template to use', 'Research free bootstrap templates and decide on preferred website design', '2.00', '2.00', 'ALL', '10', '2', '85'),
+(NULL, 'Build landing page html and CSS', NULL, '20.00', NULL, 'KM', '7', '4', '7'),
+(NULL, 'Add a coming soon page to server', 'Create the Zeus coming soon webpage', '2.00', '2.00', 'DN', '10', '1', '12'),
+(NULL, 'Tie in Google analytics to coming soon page', NULL, '1.00', '0.25', NULL, '10', '1', '12'),
+(NULL, 'Change icon to Zeus logo', 'Change website icon to Zeus company logo', '0.50', '0.25', 'DN', '10', '2', '13'),
+(NULL, 'Change colour scheme to match brand ID document', NULL, '0.50', '0.75', 'DN', '10', '2', '13'),
+(NULL, 'Add links to social media accounts to the icons at the bottom of the page', 'Add html hyperlinks', '0.50', '0.25', 'DN', '10', '2', '13'),
+(NULL, 'Make the email address field work to save email address of interested parties', NULL, '10.00', '15.00', 'DN', '10', '2', '13'),
+(NULL, 'Email validation', 'Confirm whether an actual email address has been entered or not', '2.00', '2.00', 'DN', '10', '2', '13'),
+(NULL, 'Implement AJAX solution', NULL, '5.00', '4.00', 'DN', '10', '2', '13'),
+(NULL, 'Submission status indicator', NULL, '3.00', '3.00', 'DN', '10', '2', '13'),
+(NULL, 'Change email address icon', NULL, '0.25', '0.25', 'DN', '10', '2', '13'),
+(NULL, 'Change the favicon', 'Change the favicon to the Zeus logo', '0.50', '0.25', 'DN', '10', '3', '13'),
+(NULL, 'Design and write SQL queries', 'Create queries to populate graphs', '0.25', '0.25', 'DN, NT, ES', '8', '4', '14'),
+(NULL, 'Build Dashboard html and CSS', NULL, '20.00', '5.00', 'DN', '8', '4', NULL),
+(NULL, 'We should show: a Release Burndown, multiple burndowns, velocity, admin page showing lots of graphs', NULL, '1.00', '1.00', 'ALL', '10', '1', '15'),
+(NULL, 'Find out what makes a good graph, write up guidelines, find academic references', NULL, '3.00', '1.25', 'NT', '10', '2', '16'),
+(NULL, 'Redesign database and create entity relationship diagram', Design database based on finalised entity relationship diagram', '3.00', NULL, 'NT', '10', '1', '17'),
+(NULL, 'Redesign database and create entity relationship diagram', Design database based on finalised entity relationship diagram', '3.00', NULL, 'NT', '10', '1', '18'),
+(NULL, 'Write Create script', 'Write scripts for table creation', '2.00', '1.00', 'NT, ES', '10', '1', '19'),
+(NULL, 'Write alter script', 'Write scripts for table alteration', '2.00', '0.25', 'NT, ES', '10', '1', '19'),
+(NULL, 'Write drop script', 'Write scripts to drop tables from database', '0.50', '0.25', 'NT', '10', '1', '19'),
+(NULL, 'Write insert script using our data in this spreadsheet', 'Populate database', '4.00', '2.75', 'ES', '8', '2', '20'),
+(NULL, 'Choose descriptions for status' and priorities', 'Finalise standard status and priority descriptions', '0.50', '1.00', 'ALL', '10', '2', '72'),
+(NULL, 'Create social media accounts', 'Create facebook, twitter, google + and linkedin company accounts', '1.00', '0.50', 'ES', '10', '1', '89');
+
+
+ 
+
+
+  
