96617b0010c739bdef4ac37d215809f4ed053373
Nathan Tickell <nathant1993@gmail.com>
2015-04-28 18:20:06 +0100

    New PBI's
    PBIs that have been added at a later stage of development

---
+++ SQL Scripts/Inserts/New PBI's.sql
@@ -0,0 +1,5 @@
+(NULL, 'Write Triggers for creating new databases', 'Creating extra databases', '5', '3', '1', NULL, '1'),
+(NULL, 'Come up with test data and write insert script for this', NULL, '5', '3', '1', NULL, '1'),
+(NULL, 'Trigger to populate total effort points column', NULL, '3', '3', '1', '4', '1')
+(NULL, 'Write HTML and CSS for the Dashboard inline with design work', NULL, '5', '3', '1', '4', '1');
+(NULL, 'Edit social media accounts', NULL, '1', '3', '1', '3', '1')
