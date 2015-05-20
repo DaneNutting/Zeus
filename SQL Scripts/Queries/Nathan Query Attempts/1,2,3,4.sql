67e485a77d67b92d3808ac988684d73d3470fb53
Nathan Tickell <nathant1993@gmail.com>
2015-05-20 14:57:49 +0100

    New attempt
    Everything put together just need to return one more column.

---
+++ SQL Scripts/Queries/Nathan Query Attempts/1,2,3,4.sql
@@ -0,0 +1,11 @@
+SELECT b.iteration_id, i.iteration_name, i.starting_effort, SUM(b.pbi_effort) AS 'Effort Done',
+(SELECT sum(pbi_effort) FROM backlog_items 
+WHERE iteration_id <= i.iteration_id
+AND state_id = 4) 'Running Total'
+FROM iteration i, backlog_items b
+WHERE i.iteration_id = b.iteration_id
+AND b.pbi_effort > 0
+AND b.pbi_effort IS NOT NULL
+AND i.iteration_id IS NOT NULL
+AND b.state_id = 4
+GROUP BY b.iteration_id
