f5a35022d5ebb7ca68ba591181d8d4c44e2d6705
eloises05 <s1200286@connect.glos.ac.uk>
2015-05-25 14:06:25 +0100

    doesn't work

---
+++ SQL Scripts/Queries/this is how far i got...sql
@@ -0,0 +1,14 @@
+SELECT b.iteration_id AS  'Sprint', 
+a.iteration_name AS 'Iteration Name',
+a.starting_effort AS 'Starting Effort',
+SUM( pbi_effort ) AS  'Effort Done',
+(a.starting_effort-(SELECT FROM backlog_items  
+WHERE state_id = 4)) AS 'Remaining Effort'
+FROM backlog_items b, iteration a
+WHERE a.iteration_id = b.iteration_id
+AND b.state_id =4
+AND b.iteration_id IS NOT NULL 
+AND b.iteration_id >0
+GROUP BY b.iteration_id
+
+
