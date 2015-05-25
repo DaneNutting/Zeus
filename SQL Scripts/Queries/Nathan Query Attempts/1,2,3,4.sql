e4997387cd088bbadadb53b6abf60efb32d26fbc
Nathan Tickell <nathant1993@gmail.com>
2015-05-25 14:18:23 +0100

    renamed things

--- SQL Scripts/Queries/Nathan Query Attempts/1,2,3,4.sql
+++ SQL Scripts/Queries/Nathan Query Attempts/1,2,3,4.sql
@@ -1,7 +1,7 @@
-SELECT b.iteration_id, i.iteration_name, i.starting_effort, SUM(b.pbi_effort) AS 'Effort Done',
+SELECT b.iteration_id, i.iteration_name, i.starting_effort, SUM(b.pbi_effort) AS 'Effort Done', 
 (SELECT sum(pbi_effort) FROM backlog_items 
 WHERE iteration_id <= i.iteration_id
-AND state_id = 4) 'Running Total'
+AND state_id = 4) 'effort_to_date'
 FROM iteration i, backlog_items b
 WHERE i.iteration_id = b.iteration_id
 AND b.pbi_effort > 0
@@ -9,3 +9,4 @@ AND b.pbi_effort IS NOT NULL
 AND i.iteration_id IS NOT NULL
 AND b.state_id = 4
 GROUP BY b.iteration_id
+
