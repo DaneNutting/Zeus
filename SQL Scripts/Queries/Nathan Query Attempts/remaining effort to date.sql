4884ec2ca5035a39eeee20e969e9b8cf7e6a83db
Nathan Tickell <nathant1993@gmail.com>
2015-05-27 17:12:11 +0100

    Tidied Up
    Think I'm there

--- SQL Scripts/Queries/Nathan Query Attempts/remaining effort to date.sql
+++ SQL Scripts/Queries/Nathan Query Attempts/remaining effort to date.sql
@@ -1,14 +1,22 @@
-SELECT b.iteration_id, i.iteration_name, i.starting_effort, SUM(b.pbi_effort) AS 'Effort Done', 
-(SELECT sum(pbi_effort) FROM backlog_items 
+SELECT b.iteration_id 'Sprint ID', 
+i.iteration_name 'Sprint Name', 
+i.starting_effort 'Starting Effort', 
+SUM(b.pbi_effort) AS 'Effort Done', 
+(SELECT sum(pbi_effort) 
+FROM backlog_items 
 WHERE iteration_id <= i.iteration_id
-AND state_id = 4) 'effort_to_date',
-i.starting_effort - (SELECT sum(pbi_effort) FROM backlog_items 
+AND state_id = 4) 'Effort Done To Date',
+i.starting_effort - (SELECT sum(pbi_effort) 
+FROM backlog_items 
 WHERE iteration_id <= i.iteration_id
-AND state_id = 4) 'remaining effort'
+AND state_id = 4) 'Remaining Effort'
 FROM iteration i, backlog_items b
 WHERE i.iteration_id = b.iteration_id
-AND b.pbi_effort > 0
 AND b.pbi_effort IS NOT NULL
 AND i.iteration_id IS NOT NULL
 AND b.state_id = 4
+AND i.iteration_id > (SELECT MAX(iteration_id) - 7 
+FROM iteration)
+AND i.iteration_start_date < CURDATE()
 GROUP BY b.iteration_id
+Order by b.iteration_id
