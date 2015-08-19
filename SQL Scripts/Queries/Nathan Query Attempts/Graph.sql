84e6cd06b9f73b98d3c9148830e899f47ce7631d
Nathan Tickell <nathant1993@gmail.com>
2015-08-19 19:25:21 +0100

    Query for Graph
    Without the last 10 iterations

---
+++ SQL Scripts/Queries/Nathan Query Attempts/Graph.sql
@@ -0,0 +1,19 @@
+SELECT b.iteration_id 'Sprint ID', 
+i.iteration_name 'Sprint Name', 
+i.starting_effort 'Starting Effort', 
+(SELECT sum(pbi_effort) 
+FROM backlog_items 
+WHERE iteration_id <= i.iteration_id
+AND state_id = 4) 'Effort Done To Date',
+i.starting_effort - (SELECT sum(pbi_effort) 
+					FROM backlog_items 
+					WHERE iteration_id <= i.iteration_id
+					AND state_id = 4) 'Remaining Effort'
+FROM iteration i, backlog_items b
+WHERE i.iteration_id = b.iteration_id
+AND b.pbi_effort IS NOT NULL
+AND i.iteration_id IS NOT NULL
+AND b.state_id = 4
+AND i.iteration_start_date < CURDATE()
+GROUP BY b.iteration_id
+Order by b.iteration_id
