05149c5a50ee7e8e39ec56b9daac4d01e16dcf01
Nathan Tickell <nathant1993@gmail.com>
2015-10-23 16:20:13 +0100

    Updates and tests

--- PHP/Nathan/Returning, Updating and Deleting/list_records.php
+++ PHP/Nathan/Returning, Updating and Deleting/list_records.php
@@ -9,7 +9,7 @@ $tbl_name="test"; // Table name
 mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
 mysql_select_db("$db_name")or die("cannot select DB");
 
-$sql="SELECT * FROM $tbl_name";
+$sql="SELECT * FROM $tbl_name WHERE project_id = '1'";
 $result=mysql_query($sql);
 ?>
 
@@ -22,9 +22,14 @@ $result=mysql_query($sql);
 </tr>
 
 <tr>
-<td align="center"><strong>testfield1</strong></td>
-<td align="center"><strong>testfield2</strong></td>
-<td align="center"><strong>testfield3</strong></td>
+<td align="center"><strong>pbi_id</strong></td>
+<td align="center"><strong>pbi_title</strong></td>
+<td align="center"><strong>pbi_description</strong></td>
+<td align="center"><strong>pbi_effort</strong></td>
+<td align="center"><strong>priority_id</strong></td>
+<td align="center"><strong>state_id</strong></td>
+<td align="center"><strong>iteration_id</strong></td>
+<td align="center"><strong>project_id</strong></td>
 <td align="center"><strong>Update</strong></td>
 <td align="center"><strong>Delete</strong></td>
 </tr>
@@ -34,9 +39,14 @@ while($rows=mysql_fetch_array($result)){
 ?>
 
 <tr>
-<td><? echo $rows['testfield1']; ?></td>
-<td><? echo $rows['testfield2']; ?></td>
-<td><? echo $rows['testfield3']; ?></td>
+<td><? echo $rows['pbi_id']; ?></td>
+<td><? echo $rows['pbi_title']; ?></td>
+<td><? echo $rows['pbi_description']; ?></td>
+<td><? echo $rows['pbi_effort']; ?></td>
+<td><? echo $rows['priority_id']; ?></td>
+<td><? echo $rows['state_id']; ?></td>
+<td><? echo $rows['iteration_id']; ?></td>
+<td><? echo $rows['project_id']; ?></td>
 
 <td align="center"><a href="update.php?id=<? echo $rows['id']; ?>">update</a></td>
 <td align="center"><a href="delete_ac.php?id=<? echo $rows['id']; ?>">delete</a></td>
--- SQL Scripts/Final SQL/alter table - foreign keys.sql
+++ SQL Scripts/Final SQL/alter table - foreign keys.sql
@@ -17,7 +17,8 @@ ALTER TABLE task_note
 ADD FOREIGN KEY (task_id) REFERENCES task (task_id);
 
 ALTER TABLE iteration
-ADD FOREIGN KEY  (release_id) REFERENCES releases (release_id);
+ADD FOREIGN KEY  (release_id) REFERENCES releases (release_id),
+ADD FOREIGN KEY (project_id) REFERENCES project (project_id);
 
 ALTER TABLE releases
 ADD FOREIGN KEY (project_id) REFERENCES project (project_id);
@@ -30,3 +31,7 @@ ADD FOREIGN KEY  (pbi_id) REFERENCES backlog_items (pbi_id);
 
 ALTER TABLE backlog_note
 ADD FOREIGN KEY (pbi_id) REFERENCES backlog_items (pbi_id);
+
+ALTER TABLE users_projects
+ADD FOREIGN KEY (project_id) REFERENCES projects (project_id),
+ADD FOREIGN KEY (user_id) REFERENCES test_user (user_id);
