20cf8bc9f554d31d40f3239245c7b968a75bc3b5
Nathan Tickell <nathant1993@gmail.com>
2015-10-30 16:09:28 +0000

    Current_User does not work

--- SQL Scripts/Final SQL/Audit Tables and Triggers/User's.sql
+++ SQL Scripts/Final SQL/Audit Tables and Triggers/User's.sql
@@ -12,8 +12,6 @@ user_email VARCHAR(50),
 new_user_email VARCHAR(50),
 user_password VARCHAR(20),
 new_user_password VARCHAR(20),
-user_photo BLOB,
-new_user_photo BLOB,
 user_role_id INT(10),
 new_user_role_id INT(10)
 );
@@ -24,7 +22,7 @@ FOR EACH ROW
 INSERT INTO users_audit
 (audit_user, audit_action, user_id, user_forename, user_surname, user_email, user_password, user_role_id)
 VALUES
-(CURRENT_USER(), 'Insert', NEW.user_id, NEW.user_forename, NEW.user_surname, NEW.user_email, NEW.user_password, NEW.user_role_id);
+(NULL, 'Insert', NEW.user_id, NEW.user_forename, NEW.user_surname, NEW.user_email, NEW.user_password, NEW.user_role_id);
 
 CREATE TRIGGER test_user_update_trigger
 BEFORE UPDATE ON test_user
@@ -32,7 +30,7 @@ FOR EACH ROW
 INSERT INTO users_audit
 (audit_user, audit_action, user_id, new_user_id, user_forename, new_user_forename, user_surname, new_user_surname, user_email, new_user_email, user_password, new_user_password, user_role_id, new_user_role_id)
 VALUES
-(CURRENT_USER(), 'Update', OLD.user_id, NEW.user_id, OLD.user_forename, NEW.user_forename, OLD.user_surname, NEW.user_surname, OLD.user_email, NEW.user_email, OLD.user_password, NEW.user_password, OLD.user_role_id, NEW.user_role_id);
+(NULL, 'Update', OLD.user_id, NEW.user_id, OLD.user_forename, NEW.user_forename, OLD.user_surname, NEW.user_surname, OLD.user_email, NEW.user_email, OLD.user_password, NEW.user_password, OLD.user_role_id, NEW.user_role_id);
 
 CREATE TRIGGER test_user_delete_trigger
 BEFORE DELETE ON test_user
@@ -40,4 +38,4 @@ FOR EACH ROW
 INSERT INTO users_audit
 (audit_user, audit_action, user_id, user_forename, user_surname, user_email, user_password, user_role_id)
 VALUES
-(CURRENT_USER(), 'Delete', OLD.user_id, OLD.user_forename, OLD.user_surname, OLD.user_email, OLD.user_password, OLD.user_role_id);
+(NULL, 'Delete', OLD.user_id, OLD.user_forename, OLD.user_surname, OLD.user_email, OLD.user_password, OLD.user_role_id);
