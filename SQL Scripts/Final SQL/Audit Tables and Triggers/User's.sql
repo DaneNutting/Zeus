0a5d0f6380cb57d6dfef403675c15f1ec5a1ba2f
Nathan Tickell <nathant1993@gmail.com>
2015-10-30 14:28:28 +0000

    Updated audit table for the user table we are now using

--- SQL Scripts/Final SQL/Audit Tables and Triggers/User's.sql
+++ SQL Scripts/Final SQL/Audit Tables and Triggers/User's.sql
@@ -18,26 +18,26 @@ user_role_id INT(10),
 new_user_role_id INT(10)
 );
 
-CREATE TRIGGER users_insert_trigger
-BEFORE INSERT ON users
+CREATE TRIGGER test_user_insert_trigger
+BEFORE INSERT ON test_user
 FOR EACH ROW
 INSERT INTO users_audit
-(audit_user, audit_action, user_id, user_forename, user_surname, user_email, user_password, user_photo, user_role_id)
+(audit_user, audit_action, user_id, user_forename, user_surname, user_email, user_password, user_role_id)
 VALUES
-(CURRENT_USER(), 'Insert', NEW.user_id, NEW.user_forename, NEW.user_surname, NEW.user_email, NEW.user_password, NEW.user_photo, NEW.user_role_id);
+(CURRENT_USER(), 'Insert', NEW.user_id, NEW.user_forename, NEW.user_surname, NEW.user_email, NEW.user_password, NEW.user_role_id);
 
-CREATE TRIGGER users_update_trigger
-BEFORE UPDATE ON users
+CREATE TRIGGER test_user_update_trigger
+BEFORE UPDATE ON test_user
 FOR EACH ROW
 INSERT INTO users_audit
-(audit_user, audit_action, user_id, new_user_id, user_forename, new_user_forename, user_surname, new_user_surname, user_email, new_user_email, user_password, new_user_password, user_photo, new_user_photo, user_role_id, new_user_role_id)
+(audit_user, audit_action, user_id, new_user_id, user_forename, new_user_forename, user_surname, new_user_surname, user_email, new_user_email, user_password, new_user_password, user_role_id, new_user_role_id)
 VALUES
-(CURRENT_USER(), 'Update', OLD.user_id, NEW.user_id, OLD.user_forename, NEW.user_forename, OLD.user_surname, NEW.user_surname, OLD.user_email, NEW.user_email, OLD.user_password, NEW.user_password, OLD.user_photo, NEW.user_photo, OLD.user_role_id, NEW.user_role_id);
+(CURRENT_USER(), 'Update', OLD.user_id, NEW.user_id, OLD.user_forename, NEW.user_forename, OLD.user_surname, NEW.user_surname, OLD.user_email, NEW.user_email, OLD.user_password, NEW.user_password, OLD.user_role_id, NEW.user_role_id);
 
-CREATE TRIGGER users_delete_trigger
-BEFORE DELETE ON users
+CREATE TRIGGER test_user_delete_trigger
+BEFORE DELETE ON test_user
 FOR EACH ROW
 INSERT INTO users_audit
-(audit_user, audit_action, user_id, user_forename, user_surname, user_email, user_password, user_photo, user_role_id)
+(audit_user, audit_action, user_id, user_forename, user_surname, user_email, user_password, user_role_id)
 VALUES
-(CURRENT_USER(), 'Delete', OLD.user_id, OLD.user_forename, OLD.user_surname, OLD.user_email, OLD.user_password, OLD.user_photo, OLD.user_role_id);
+(CURRENT_USER(), 'Delete', OLD.user_id, OLD.user_forename, OLD.user_surname, OLD.user_email, OLD.user_password, OLD.user_role_id);
